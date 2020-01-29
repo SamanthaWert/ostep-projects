7d11f3c70001e458922fdf86abbd861e5874b632
SamanthaWert <59936450+SamanthaWert@users.noreply.github.com>
2020-01-29 18:33:33 -0500

    Changed to strstr()

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -7,24 +7,15 @@ int main(int argc, char *argv[]){
         exit(1);
     }
     else if(argc == 2){
-        char term = argv[1];
-        char search[sizeof(term)] = term;
-        
-        int lensearch = sizeof(search);
-        char str[1];
+        char *str;
         scanf("%s", str);
-        for(int j = 0; j<sizeof(str); j++){
-            if(str[j] == search[0]){
-                int k=0;
-                while(k<lensearch){
-                    if(str[j+k] != search[k]){
-                        break;
-                    }
-                    if(k == lensearch-1){
-                        printf("%s", str);
-                    }
-                    k++;
-                }
+        char *search = argv[1];
+        char *line;
+        int numchar;
+        while((numchar = getline(*line, 0, fp)) != -1){
+            char *found = strstr(line, search);
+            if(found != NULL){
+                printf("%p", line);
             }
         }
     }
@@ -50,24 +41,13 @@ int main(int argc, char *argv[]){
                 printf("wcat: cannot open file\n");
                 exit(1);
             }
-            char search[] = argv[1];
-            int lensearch = sizeof(search);
-            char *line[] = "";
+            char *search = argv[1];
+            char *line;
             int numchar;
-            while((numchar = getline(line, 0, fp)) != -1){
-                for(int j = 0; j<numchar; j++){
-                    if(line[j] == search[0]){
-                        int k=0;
-                        while(k<lensearch){
-                            if(line[j+k] != search[k]){
-                                break;
-                            }
-                            if(k == lensearch-1){
-                                printf("%s", line);
-                            }
-                            k++;
-                        }
-                    }
+            while((numchar = getline(*line, 0, fp)) != -1){
+                char *found = strstr(line, search);
+                if(found != NULL){
+                    printf("%p", line);
                 }
             }            
             fclose(fp);
