ff2328bdf1c9c3bd8522f3068df893cbaa9b90f1
SamanthaWert <59936450+SamanthaWert@users.noreply.github.com>
2020-01-29 16:34:47 -0500

    fixed empty string error

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -29,12 +29,19 @@ int main(int argc, char *argv[]){
         }
     }
     else{
-        if(argv[1] == ""){
-            char buffer[1000];
-            while(fgets(buffer, 1000, fp)){
-                printf("%s", buffer);
+        if(argv[1] == ""){     
+            for(int i=3; i<argc; i++){
+                FILE *fp = fopen(argv[i+1], "r");
+                if (fp == NULL) {
+                    printf("wcat: cannot open file\n");
+                    exit(1);
+                }
+                char buffer[1000];
+                while(fgets(buffer, 1000, fp)){
+                    printf("%s", buffer);
+                }
+                fclose(fp);
             }
-            fclose(fp);
             exit(0);
         }
         for(int i = 3; i<argc; i++){
