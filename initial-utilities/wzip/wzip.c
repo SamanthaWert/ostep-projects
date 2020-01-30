3a2822bbdbcebd8396e9f085c571b052200bf75f
SamanthaWert <59936450+SamanthaWert@users.noreply.github.com>
2020-01-29 22:58:47 -0500

    changed function name

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int counter(const char* line, char c)
+int counts(const char* line, char c);
 
-    int main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
     int counter[26]; //where fwrite will read from
     size_t items = 0; //number of items of data
     size_t size = 5; //four byte binary and char
@@ -14,32 +14,32 @@ int counter(const char* line, char c)
         char *line = NULL;
         size_t length = 0;
         while((numchar = getline(&line, &length, fb)) != -1){
-            counter[0] = counter[0] + counter(line, "a");
-            counter[1] = counter[1] + counter(line, "b");
-            counter[2] = counter[2] + counter(line, "c");
-            counter[3] = counter[3] + counter(line, "d");
-            counter[4] = counter[4] + counter(line, "e");
-            counter[5] = counter[5] + counter(line, "f");
-            counter[6] = counter[6] + counter(line, "g");
-            counter[7] = counter[7] + counter(line, "h");
-            counter[8] = counter[8] + counter(line, "i");
-            counter[9] = counter[9] + counter(line, "j");
-            counter[10] = counter[10] + counter(line, "k");
-            counter[11] = counter[11] + counter(line, "l");
-            counter[12] = counter[12] + counter(line, "m");
-            counter[13] = counter[13] + counter(line, "n");
-            counter[14] = counter[14] + counter(line, "o");
-            counter[15] = counter[15] + counter(line, "p");
-            counter[16] = counter[16] + counter(line, "q");
-            counter[17] = counter[17] + counter(line, "r");
-            counter[18] = counter[18] + counter(line, "s");
-            counter[19] = counter[19] + counter(line, "t");
-            counter[20] = counter[20] + counter(line, "u");
-            counter[21] = counter[21] + counter(line, "v");
-            counter[22] = counter[22] + counter(line, "w");
-            counter[23] = counter[23] + counter(line, "x");
-            counter[24] = counter[24] + counter(line, "y");
-            counter[25] = counter[25] + counter(line, "z");
+            counter[0] = counter[0] + counts(line, "a");
+            counter[1] = counter[1] + counts(line, "b");
+            counter[2] = counter[2] + counts(line, "c");
+            counter[3] = counter[3] + counts(line, "d");
+            counter[4] = counter[4] + counts(line, "e");
+            counter[5] = counter[5] + counts(line, "f");
+            counter[6] = counter[6] + counts(line, "g");
+            counter[7] = counter[7] + counts(line, "h");
+            counter[8] = counter[8] + counts(line, "i");
+            counter[9] = counter[9] + counts(line, "j");
+            counter[10] = counter[10] + counts(line, "k");
+            counter[11] = counter[11] + counts(line, "l");
+            counter[12] = counter[12] + counts(line, "m");
+            counter[13] = counter[13] + counts(line, "n");
+            counter[14] = counter[14] + counts(line, "o");
+            counter[15] = counter[15] + counts(line, "p");
+            counter[16] = counter[16] + counts(line, "q");
+            counter[17] = counter[17] + counts(line, "r");
+            counter[18] = counter[18] + counts(line, "s");
+            counter[19] = counter[19] + counts(line, "t");
+            counter[20] = counter[20] + counts(line, "u");
+            counter[21] = counter[21] + counts(line, "v");
+            counter[22] = counter[22] + counts(line, "w");
+            counter[23] = counter[23] + counts(line, "x");
+            counter[24] = counter[24] + counts(line, "y");
+            counter[25] = counter[25] + counts(line, "z");
         }
     }
             counter[0] = counter[0] + "a";
@@ -72,7 +72,7 @@ int counter(const char* line, char c)
     fwrite(counter, size, items, fp);
 }
 
-int counter(const char* line, char c){
+int counts(const char* line, char c){
     int times = 0;
     int linelen = strlen(line);
     for(int i=0; i<linelen; i++){
